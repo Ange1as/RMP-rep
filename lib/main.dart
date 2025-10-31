ca27425d1a9c5f8014ff57fcc8c4ce9f9a403693
Ange1as <145800473+Ange1as@users.noreply.github.com>
2025-10-31 08:36:29 +0000

    feat: добавлена интеграция с Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'pages/home_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Инициализация Supabase с временными данными (будет обновлено на странице)
+  await Supabase.initialize(
+    url: 'https://frvexfoezbscdbcvuxas.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZydmV4Zm9lemJzY2RiY3Z1eGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NDY4ODgsImV4cCI6MjA3NTMyMjg4OH0.XDr9MFxBMX0P42a4MwjstxtZeh_Caqdyrfpfr7d9ec8',
+  );
+
   runApp(const MyApp());
 }
 
@@ -9,80 +20,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Лабораторная работа №1',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Базовый UI'),
-      ),
-      body: Column(
-        children: [
-          // Первый Container
-          Container(
-            width: 200,
-            height: 100,
-            color: Colors.red,
-          ),
-
-          // Row с тремя Text
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Text('Текст 1'),
-              Text('Текст 2'),
-              Text('Текст 3'),
-            ],
-          ),
-
-          // Второй Container
-          Container(
-            width: 150,
-            height: 80,
-            color: Colors.green,
-          ),
-
-          // Expanded с Row и CircleAvatar
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.blue,
-                ),
-                CircleAvatar(
-                  radius: 40,
-                  backgroundImage: NetworkImage(
-                    'https://png.pngtree.com/thumb_back/fw800/background/20230617/pngtree-background-artwork-black-and-white-contoured-paper-design-in-3d-rendering-image_3616737.jpg',
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Button pressed!');
-        },
-        child: const Icon(Icons.add),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
